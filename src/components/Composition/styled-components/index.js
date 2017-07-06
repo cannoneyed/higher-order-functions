4ed19cf73bd387c368e0b2d1d9b463ab659896e2
Andy Coenen <andrew.coenen@gmail.com>
2017-07-05 23:58:33 -0700

    Update styles

---
+++ src/components/Composition/styled-components/index.js
@@ -0,0 +1,42 @@
+import styled, { css } from 'styled-components'
+
+export const StageWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+`
+
+const hover = css`
+  background-color: #f42b2e;
+  transform: scale3d(1.1, 1.1, 1)
+`
+
+const active = css`
+  transform: scale3d(4.5, 4.5, 1);
+`
+
+export const StageButton = styled.div`
+  backface-visibility: hidden;
+  cursor: ${props => (props.isActive ? '' : 'pointer')};
+  width: ${props => props.size}px;
+  height: ${props => props.size + 1}px;
+  position: absolute;
+  z-index: 999;
+
+  ${props => (props.isActive ? active : '')};
+
+  &:hover {
+    ${props => (props.isActive ? '' : hover)};
+  }
+
+  transition: all ${props => (props.isActive ? 1200 : 250)}ms
+    cubic-bezier(0.000, 1.020, 0.510, 0.950);
+`
+
+export const Stage = styled.div`
+  width: 100%;
+  height: 100%;
+`
